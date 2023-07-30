03d119662384eb5b14d4f21b8bf1131a8d750993
Kaan Dai <79655072+KDAI19@users.noreply.github.com>
2023-07-30 16:14:16 +0300

    gereksiz branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import Sidebar from './Sidebar';
-
-const CanvasComponent = React.lazy(() => import('./CanvasComponent'));
+import React, { Suspense } from "react";
+import Sidebar from "./Sidebar";
+import CanvasComponent from "./CanvasComponent";
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <Sidebar />
-            <Suspense fallback={<div>Loading...</div>}>
-                <CanvasComponent />
-            </Suspense>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Sidebar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CanvasComponent />
+      </Suspense>
+    </div>
+  );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,60 +1,39 @@
-import React, { useState } from 'react';
-import leftArrow from './arrowImage1.png';
-import rightArrow from './arrowImage2.png';
+import React, { useState } from "react";
+import './App.css';
 
 function Sidebar() {
-    const [leftMenuActive, setLeftMenuActive] = useState(false);
-    const [rightMenuActive, setRightMenuActive] = useState(false);
-    const leftItems = ["Left Item 1", "Left Item 2", "Left Item 3", "Left Item 4"]; // list of draggable items for the left sidebar
-    const rightItems = ["Right Item 1", "Right Item 2", "Right Item 3", "Right Item 4"]; // list of draggable items for the right sidebar
+  const [isLeftOpen, setLeftOpen] = useState(false);
+  const [isRightOpen, setRightOpen] = useState(false);
 
-    const handleLeftButtonClick = () => {
-        setLeftMenuActive(!leftMenuActive);
-    };
+  const leftItems = ["Rectangle", "Circle", "Triangle"];
+  const rightItems = ["Red", "Green", "Blue"];
 
-    const handleRightButtonClick = () => {
-        setRightMenuActive(!rightMenuActive);
-    };
+  const onDragStart = (event, item) => {
+    console.log(item); // Placeholder function. You'll need to replace this with your actual function.
+  }
 
-    const onDragStart = (event, text) => {
-        event.dataTransfer.setData("text/plain", text);
-    };
-
-    return (
-        <>
-            {/* Left Sidebar */}
-            <div className={`sidebar leftSidebar ${leftMenuActive ? 'open' : ''}`}>
-                <button onClick={handleLeftButtonClick}>
-                    <img src={leftArrow} alt='Arrow'/>
-                </button>
-                {leftMenuActive && (
-                    <div className="content">
-                        {leftItems.map((item, index) => (
-                            <p key={index} draggable onDragStart={(event) => onDragStart(event, item)}>
-                                {item}
-                            </p>
-                        ))}
-                    </div>
-                )}
-            </div>
-
-            {/* Right Sidebar */}
-            <div className={`sidebar rightSidebar ${rightMenuActive ? 'open' : ''}`}>
-                <button onClick={handleRightButtonClick}>
-                    <img src={rightArrow} alt='Arrow'/>
-                </button>
-                {rightMenuActive && (
-                    <div className="content">
-                        {rightItems.map((item, index) => (
-                            <p key={index}>
-                                {item}
-                            </p>
-                        ))}
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={`sidebar leftSidebar ${isLeftOpen ? "open" : ""}`}>
+        {leftItems.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+        <button onClick={() => setLeftOpen(!isLeftOpen)}>
+          {isLeftOpen ? "Close" : "Open"}
+        </button>
+      </div>
+      <div className={`sidebar rightSidebar ${isRightOpen ? "open" : ""}`}>
+        {rightItems.map((item, index) => (
+          <p key={index} draggable onDragStart={(event) => onDragStart(event, item)}>
+            {item}
+          </p>
+        ))}
+        <button onClick={() => setRightOpen(!isRightOpen)}>
+          {isRightOpen ? "Close" : "Open"}
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Sidebar;
